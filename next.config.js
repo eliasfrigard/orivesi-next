b01cd7c1ade45b7682e2b0cf760f90b57ed17a04
eliasfrigard <elias_frigard@hotmail.com>
2022-07-11 21:25:34 +0300

    added envs to client

--- next.config.js
+++ next.config.js
@@ -1,6 +1,10 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  env: {
+    API_ADDRESS: process.env.API_ADDRESS,
+    API_TOKEN: process.env.API_TOKEN,
+  },
 }
 
 module.exports = nextConfig
