d83d02a84d8eff4585cdb6a6c2cc4a88ef434efb
Elias Frigård <eliasfrigard@pop-os.localdomain>
2022-07-29 11:19:19 +0300

    mobile margins on eventhighlights

--- components/Modules/EventHighlights.js
+++ components/Modules/EventHighlights.js
@@ -4,7 +4,7 @@ import Title from '../Title'
 
 export default function EventHighlights({ events }) {
   return (
-    <div className='container flex flex-col	items-center mt-32 mb-16 text-grey-500'>
+    <div className='container flex flex-col	items-center mt-16 md:mt-32 md:mb-16 text-grey-500'>
       <Title version='v1'>SEURAAVAT TAPAHTUMAT</Title>
 
       <div className='flex w-full flex-wrap flex-column justify-center gap-10 items-center my-16 sm:mb-20'>
