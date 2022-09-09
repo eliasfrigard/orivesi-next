a10af6a5800a265130b33082877ae5a02435aee8
eliasfrigard <elias_frigard@hotmail.com>
2022-09-09 20:25:23 +0300

    images do not take up too much space

--- components/Modules/HighlightText.js
+++ components/Modules/HighlightText.js
@@ -2,18 +2,17 @@ import Image from 'next/image'
 
 export default function HighlightText({ title, subtitle }) {
   return (
-    <div className='min-h-[100vh] w-[100vw] overflow-hidden'>
-      <div className='relative h-[100%] w-[100vw]'>
-        <Image
-          className='object-bottom'
-          alt='top-slice'
-          src='/top-slice.svg'
-          height='100%'
-          width='100vw'
-          layout='responsive'
-          objectFit='contain'
-        />
-      </div>
+    <div className='w-[100vw] overflow-hidden'>
+      <Image
+        style={{ position: 'unset !important' }}
+        className='object-top'
+        alt='top-slice'
+        src='/top-slice.svg'
+        height='22%'
+        width='100vw'
+        layout='responsive'
+        objectFit='contain'
+      />
       <div className='py-10 sm:pt-4 lg:pt-24 lg:pb-32 text-center w-full px-4'>
         <h1 className='text-4xl lg:text-[9rem] lg:leading-[10rem] font-bold font-sketch text-accent-500 drop-shadow-lg'>
           {title}
@@ -26,10 +25,10 @@ export default function HighlightText({ title, subtitle }) {
       </div>
       <div className='relative h-[100%] w-[100vw]'>
         <Image
-          className='object-top'
+          className='object-bottom'
           alt='top-slice'
           src='/bottom-slice.svg'
-          height='100%'
+          height='56%'
           width='100vw'
           layout='responsive'
           objectFit='contain'
