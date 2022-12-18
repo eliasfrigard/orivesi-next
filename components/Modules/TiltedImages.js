9ffcccd0c1be87d1cda91497b98a13c56a406155
eliasfrigard <elias_frigard@hotmail.com>
2022-12-18 18:44:39 +0200

    removed unused props

--- components/Modules/TiltedImages.js
+++ components/Modules/TiltedImages.js
@@ -15,62 +15,27 @@ export default function ShortPresentation() {
       <div className='-my-4 flex justify-center gap-5 overflow-hidden py-4 sm:gap-8'>
         <AnimateIn opacityDuration={1000}>
           <div className='relative aspect-[9/10] w-44 flex-none overflow-hidden rounded-xl sm:w-72 sm:rounded-2xl rotate-2 shadow-md'>
-            <Image
-              alt='top-slice'
-              src='/tilted-images/2.webp'
-              height='100%'
-              width='100%'
-              layout='fill'
-              objectFit='cover'
-            />
+            <Image alt='top-slice' src='/tilted-images/2.webp' layout='fill' objectFit='cover' />
           </div>
         </AnimateIn>
         <AnimateIn delay={200} opacityDuration={1000}>
           <div className='relative aspect-[9/10] w-44 flex-none overflow-hidden rounded-xl sm:w-72 sm:rounded-2xl -rotate-2 shadow-md'>
-            <Image
-              alt='top-slice'
-              src='/tilted-images/1.webp'
-              height='100%'
-              width='100%'
-              layout='fill'
-              objectFit='cover'
-            />
+            <Image alt='top-slice' src='/tilted-images/1.webp' layout='fill' objectFit='cover' />
           </div>
         </AnimateIn>
         <AnimateIn delay={400} opacityDuration={1000}>
           <div className='relative aspect-[9/10] w-44 flex-none overflow-hidden rounded-xl sm:w-72 sm:rounded-2xl rotate-2 shadow-md'>
-            <Image
-              alt='top-slice'
-              src='/tilted-images/4.webp'
-              height='100%'
-              width='100%'
-              layout='fill'
-              objectFit='cover'
-            />
+            <Image alt='top-slice' src='/tilted-images/4.webp' layout='fill' objectFit='cover' />
           </div>
         </AnimateIn>
         <AnimateIn delay={600} opacityDuration={1000}>
           <div className='relative aspect-[9/10] w-44 flex-none overflow-hidden rounded-xl sm:w-72 sm:rounded-2xl rotate-2 shadow-md'>
-            <Image
-              alt='top-slice'
-              src='/tilted-images/3.webp'
-              height='100%'
-              width='100%'
-              layout='fill'
-              objectFit='cover'
-            />
+            <Image alt='top-slice' src='/tilted-images/3.webp' layout='fill' objectFit='cover' />
           </div>
         </AnimateIn>
         <AnimateIn delay={800} opacityDuration={1000}>
           <div className='relative aspect-[9/10] w-44 flex-none overflow-hidden rounded-xl sm:w-72 sm:rounded-2xl -rotate-2 shadow-md'>
-            <Image
-              alt='top-slice'
-              src='/tilted-images/5.webp'
-              height='100%'
-              width='100%'
-              layout='fill'
-              objectFit='cover'
-            />
+            <Image alt='top-slice' src='/tilted-images/5.webp' layout='fill' objectFit='cover' />
           </div>
         </AnimateIn>
       </div>
