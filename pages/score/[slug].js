803d1d36c23d0de7eeb7dae03377b9f2caa27ca7
Elias Frigård <elias.frigard@tvtools.fi>
2022-12-22 20:54:33 +0200

    score page score margins

--- pages/score/[slug].js
+++ pages/score/[slug].js
@@ -72,7 +72,7 @@ export default function ScorePage({ score }) {
           <div className='flex flex-col gap-10 lg:gap-16'>
             <div className='flex flex-col lg:flex-row gap-10'>
               {score.Scores.data && score.Scores.data.length > 0 ? (
-                <div className={`flex flex-col w-full ${score.Audio.data ? 'lg:w-3/5' : 'lg:w-full'} gap-6`}>
+                <div className={`flex flex-col w-full ${score.Audio.data ? 'lg:w-3/5' : 'lg:w-full'} gap-4`}>
                   {score.Scores.data.map((file) => (
                     <a href={file.attributes.url} key={file.attributes.id} target='_blank' rel='noreferrer'>
                       <div className='selection:bg-accent-500 flex gap-4 items-center w-full bg-secondary-500 text-white shadow-lg cursor-pointer hover:shadow-xl hover:bg-accent-500 duration-150 rounded-lg py-4 px-6'>
