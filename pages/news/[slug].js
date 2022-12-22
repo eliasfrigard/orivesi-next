fcd84b0412e083de14cf6f5e4827f3bf652df850
Elias Frigård <elias.frigard@tvtools.fi>
2022-12-22 20:31:31 +0200

    news page margin update

--- pages/news/[slug].js
+++ pages/news/[slug].js
@@ -60,7 +60,7 @@ export default function NewsPage({ id, post }) {
           </div>
         </div>
         {post.Youtube ? (
-          <div className='w-[90vw] xl:w-[60vw] mb-10 lg:mb-16 m-10 lg:m-16 aspect-79/52 overflow-hidden rounded-xl shadow-xl'>
+          <div className='w-[90vw] xl:w-[60vw] mb-10 lg:mb-16 m-10 lg:m-10 aspect-79/52 overflow-hidden rounded-xl shadow-xl'>
             <iframe
               className='w-full aspect-79/52'
               src={`https://www.youtube.com/embed/${post.Youtube}`}
