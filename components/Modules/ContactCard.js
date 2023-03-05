2868addc4da03c782352513fadf50fe50ff7987b
Elias Frigård <elias.frigard@tvtools.fi>
2023-03-05 18:13:55 +0200

    update contact card

--- components/Modules/ContactCard.js
+++ components/Modules/ContactCard.js
@@ -1,9 +1,9 @@
 export default function ContactInfo({ name, role, phone, email }) {
   return (
-    <div className='w-[330px] h-[220px] flex flex-col gap-2.5 items-center justify-center text-center p-6 rounded-lg border-4 border-accent-500 shadow-md tracking-wide'>
-      <p className='text-2xl font-round '>{name}</p>
-      <p className='font-bold font-cursive text-2xl'>{role}</p>
-      <a href={`mailto:${email}`} className='underline font-medium text-secondary-800'>
+    <div className='w-[330px] h-[230px] flex flex-col gap-2.5 items-center justify-center text-center p-6 rounded-lg border border-secondary-800 shadow tracking-wide text-secondary-800'>
+      <p className='text-2xl font-round'>{name}</p>
+      <p className='font-cursive text-2xl'>{role}</p>
+      <a href={`mailto:${email}`} className='underline font-medium text-accent-800'>
         {email}
       </a>
       <p className='font-medium'>{phone}</p>
