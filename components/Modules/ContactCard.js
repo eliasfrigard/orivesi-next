13890953137e5da07bc05991479f25b9dcc8ba3b
eliasfrigard <elias_frigard@hotmail.com>
2023-04-14 22:32:03 +0300

    contact card color

--- components/Modules/ContactCard.js
+++ components/Modules/ContactCard.js
@@ -1,9 +1,9 @@
 export default function ContactInfo({ name, role, phone, email }) {
   return (
-    <div className='w-[330px] h-[230px] flex flex-col gap-2.5 items-center justify-center text-center p-6 rounded-lg border border-secondary-800 shadow tracking-wide text-secondary-800'>
+    <div className='w-[330px] h-[230px] flex flex-col gap-2.5 items-center justify-center text-center p-6 rounded-lg border border-secondary-500 shadow tracking-wide text-secondary-700'>
       <p className='text-2xl font-round'>{name}</p>
       <p className='font-cursive text-2xl'>{role}</p>
-      <a href={`mailto:${email}`} className='underline font-medium text-accent-800'>
+      <a href={`mailto:${email}`} className='underline font-medium text-accent-700'>
         {email}
       </a>
       <p className='font-medium'>{phone}</p>
