a47c46dd0b30ec45c66d050177d7eb28cfb49bff
eliasfrigard <elias_frigard@hotmail.com>
2023-04-14 20:45:43 +0300

    text highlight redo

--- components/Modules/HighlightText.js
+++ components/Modules/HighlightText.js
@@ -5,13 +5,11 @@ export default function HighlightText({ title, subtitle }) {
     <AnimateIn delay={1000} opacityDuration={1000} className='hidden md:block'>
       <div className='pb-16 sm:pt-4 lg:pt-24 lg:pb-24 text-center w-full px-4'>
         {subtitle ? (
-          <h2 className='text-2xl sm:text-4xl leading-10 sm:leading-tight my-4 mb-6 font-work text-grey-300 uppercase drop-shadow-sm'>
+          <h2 className='text-2xl sm:text-3xl font-medium leading-10 sm:leading-loose mb-6 font-work text-secondary-600 uppercase drop-shadow-sm'>
             {subtitle}
           </h2>
         ) : null}
-        <h1 className='text-7xl lg:text-[9rem] lg:leading-[10rem] font-bold font-sketch text-accent-500 drop-shadow-lg'>
-          {title}
-        </h1>
+        <h1 className='text-7xl lg:text-9xl font-bold font-sketch text-accent-600 drop-shadow-lg'>{title}</h1>
       </div>
     </AnimateIn>
   )
