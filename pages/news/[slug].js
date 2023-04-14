5f126a6cc3ec2b7b06ed62e7cb635f813f6121db
eliasfrigard <elias_frigard@hotmail.com>
2023-04-14 21:25:57 +0300

    better news page

--- pages/news/[slug].js
+++ pages/news/[slug].js
@@ -11,7 +11,6 @@ import EventPreview from '../../components/Modules/EventPreview'
 
 import { BsPerson } from 'react-icons/bs'
 import { BiTimeFive } from 'react-icons/bi'
-import { AiOutlineMail, AiOutlineFacebook, AiOutlineLink, AiOutlinePrinter } from 'react-icons/ai'
 import { FiChevronRight } from 'react-icons/fi'
 import { MdOutlineArticle } from 'react-icons/md'
 
@@ -27,9 +26,9 @@ export default function NewsPage({ id, post }) {
       pageImage={post.Images.data[0].attributes.url}
       pageUrl={`/news/${id}`}
     >
-      <div className='container flex my-8 lg:my-24 flex-col items-center'>
-        <div className='md:container lg:w-[925px] max-w-full'>
-          <div className='absolute flex items-center gap-3 text-md mt-[-3rem] text-grey-300'>
+      <div className='container flex my-12 pt-[53px] flex-col items-center'>
+        <div className='md:container max-w-full flex flex-col gap-4 md:gap-5'>
+          <div className='flex items-center gap-3 text-md mt-[-3rem] text-grey-300'>
             <div className='flex items-center gap-2 hover:text-grey-800 hover:font-medium duration-75'>
               <MdOutlineArticle />
               <Link href='/news'>Uutiset</Link>
@@ -42,23 +41,25 @@ export default function NewsPage({ id, post }) {
           </div>
 
           <Title version='v3'>{post.Title}</Title>
-          <div className='meta flex flex-col lg:flex-row gap-4 lg:gap-6 items-start mt-5'>
-            <div className='flex gap-4 lg:gap-2 items-center justify-center'>
-              <BiTimeFive className='text-2xl' />
+
+          <div className='meta flex flex-col lg:flex-row gap-4 lg:gap-6 items-start text-accent-600'>
+            <div className='flex gap-4 lg:gap-2 items-center justify-center text-accent-600'>
+              <BiTimeFive className='text-xl text-secondary-600' />
               <Moment className='font-work text-lg' format={'LL '}>
                 {post.createdAt}
               </Moment>
             </div>
             <div className='flex gap-4 lg:gap-2 items-center justify-center'>
-              <BsPerson className='text-2xl' />
+              <BsPerson className='text-xl text-secondary-600' />
               <p className='font-work text-lg'>{post.Author}</p>
             </div>
           </div>
         </div>
+
         {post.Youtube ? (
-          <div className='w-[90vw] xl:w-[60vw] mb-10 lg:mb-12 m-10 lg:m-10 aspect-79/52 overflow-hidden rounded-xl shadow-xl'>
+          <div className='w-full my-10 aspect-video overflow-hidden rounded-xl shadow-xl'>
             <iframe
-              className='w-full aspect-79/52'
+              className='w-full aspect-video'
               src={`https://www.youtube.com/embed/${post.Youtube}`}
               title='YouTube video player'
               frameBorder={0}
@@ -68,37 +69,27 @@ export default function NewsPage({ id, post }) {
             ></iframe>
           </div>
         ) : (
-          <div className='w-[90vw] xl:w-[60vw] mb-10 lg:mb-12 m-10 lg:m-10 aspect-79/52 img relative shadow-md'>
+          <div className='container w-full my-10 aspect-79/52 img relative shadow-md'>
             <Image
-              className='rounded'
+              className='rounded-lg'
               loader={myLoader}
               src={post.Images.data[0].attributes.url}
               alt={post.Images.data[0].attributes.alternativeText}
-              width='100%'
-              height='100%'
               layout='fill'
               objectFit='cover'
             />
           </div>
         )}
-        <div className='flex flex-col lg:flex-row gap-6 lg:gap-16'>
-          {/* SIDEBAR IS DISABLED FOR NOW UNTIL BETTER CONTENT. */}
-          {/* <div className='flex lg:flex-col gap-5 lg:mt-[5px] my-2 lg:my-0'>
-            <AiOutlineLink className='text-3xl' />
-            <AiOutlineMail className='text-3xl' />
-            <AiOutlinePrinter className='text-3xl' />
-            <AiOutlineFacebook className='text-3xl' />
-          </div> */}
-          <div className='md:container md:border-2 border-secondary-500 rounded-xl md:px-8 lg:px-16 md:py-12 lg:py-16 md:shadow-xl'>
-            <div
-              className='prose max-w-3xl xl:prose-lg leading-[2.1rem]'
-              dangerouslySetInnerHTML={{ __html: md().render(post.Text) }}
-            />
-          </div>
+        <div className='flex flex-col lg:flex-row gap-6 lg:gap-16 justify-center items-center md:border border-secondary-500 border-opacity-40 rounded-xl md:shadow md:px-8 lg:px-12 md:py-12 lg:py-12'>
+          <div
+            className='prose max-w-4xl leading-[2.1rem]'
+            dangerouslySetInnerHTML={{ __html: md().render(post.Text) }}
+          />
         </div>
       </div>
+
       {/* Associated Scores */}
-      {post.music_scores.data.length > 0 ? (
+      {post.music_scores.data.length > 0 && (
         <div className='container my-16 md:my-24'>
           <Title version='v2'>Liittyviä nuotteja.</Title>
 
@@ -114,14 +105,11 @@ export default function NewsPage({ id, post }) {
             ))}
           </div>
         </div>
-      ) : (
-        ''
       )}
-      {/* Associated Events */}
-      {post.events.data.length > 0 ? (
-        <div className='container my-12 lg:mt-24 lg:mb-32'>
-          <Title version='v2'>Liittyviä tapahtumia.</Title>
 
+      {/* Associated Events */}
+      {post.events.data.length > 0 && (
+        <div className='container my-12 lg:mb-24'>
           <div className='max-w-[1400px] flex flex-wrap gap-10 justify-center items-center my-8 lg:my-16'>
             {post.events.data.map((event) => (
               <EventPreview
@@ -136,8 +124,6 @@ export default function NewsPage({ id, post }) {
             ))}
           </div>
         </div>
-      ) : (
-        ''
       )}
     </Layout>
   )
