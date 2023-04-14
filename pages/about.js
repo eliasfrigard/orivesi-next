2e27624d6e84c0cdb2567281018246f54784da13
eliasfrigard <elias_frigard@hotmail.com>
2023-04-14 20:16:48 +0300

    redo about page

--- pages/about.js
+++ pages/about.js
@@ -2,7 +2,7 @@ import axios from 'axios'
 import Image from 'next/image'
 import md from 'markdown-it'
 
-import Title from '../components/Title'
+import AnimateIn from '../components/AnimateIn'
 import Layout from '../components/Layout'
 
 export default function About({ about }) {
@@ -12,38 +12,24 @@ export default function About({ about }) {
 
   return (
     <Layout>
-      <div className='container flex flex-col mb-24 mt-2 lg:mt-16 items-center'>
-        <div className='hidden lg:block'>
-          <h3 className='text-6xl font-sketch uppercase font-bold tracking-wider text-center mb-4'>
-            {about.Supertitle}
-          </h3>
-          <Title>{about.Title}</Title>
-        </div>
-
-        <div className='lg:hidden'>
-          <Title>{about.Supertitle + ' ' + about.Title}</Title>
-        </div>
-
-        <div distance={0} className='w-[90vw] xl:w-[60vw] my-16 aspect-79/52 img relative shadow-xl'>
-          <Image
-            className='rounded-lg'
-            loader={myLoader}
-            src={about.Images.data[0].attributes.url}
-            alt={about.Images.data[0].attributes.alternativeText}
-            width='100%'
-            height='100%'
-            layout='fill'
-            objectFit='cover'
-          />
-        </div>
-
-        <div className='flex gap-16'>
-          <div
-            className='prose max-w-3xl xl:prose-lg leading-[2.1rem]'
-            dangerouslySetInnerHTML={{ __html: md().render(about.Text) }}
-          />
-        </div>
+      <div distance={0} className='w-[90vw] xl:w-full aspect-79/52 img shadow relative -mt-[135px]'>
+        <Image
+          loader={myLoader}
+          src={about.Images.data[0].attributes.url}
+          alt={about.Images.data[0].attributes.alternativeText}
+          layout='fill'
+          objectFit='cover'
+        />
+        <AnimateIn>
+          <h1 className='hidden lg:block absolute text-white font-bold font-sketch opacity-80 text-6xl xl:text-8xl text-center top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2'>
+            Suomen suurin pelimanniorkesteri!
+          </h1>
+        </AnimateIn>
       </div>
+      <div
+        className='container mb-24 mt-2 lg:mt-16 items-center prose max-w-4xl leading-loose prose-img:roundedShadow font-work'
+        dangerouslySetInnerHTML={{ __html: md().render(about.Text) }}
+      />
     </Layout>
   )
 }
