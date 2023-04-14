c1a763d0e8439d378323ea54f2370be4492c2352
eliasfrigard <elias_frigard@hotmail.com>
2023-04-14 20:23:38 +0300

    redo button

--- components/Button.js
+++ components/Button.js
@@ -7,33 +7,34 @@ export default function Button({
   color = 'bg-accent-500',
   hoverColor = 'hover:bg-accent-500',
   rounded = 'rounded-full',
-  width = 'min-w-[230px]',
+  width = 'min-w-[200px]',
   externalUrl = false,
   height,
   disabled = false,
 }) {
   return url ? (
-    (
-      externalUrl ?
-        <a target="_blank" href={url} rel="noopener noreferrer">
-          <button
-            className={`${width} ${color} ${hoverColor} text-white font-medium py-4 px-6 ${rounded} tracking-wider text-lg hover:scale-105 duration-150 active:scale-100 active:${color} uppercase font-sketch font-bold shadow-lg hover:shadow-xl`}
-          >
-            {children}
-          </button>
-        </a> :
-        <Link href={url}>
-          <button
-            className={`${width} ${color} ${hoverColor} text-white font-medium py-4 px-6 ${rounded} tracking-wider text-lg hover:scale-105 duration-150 active:scale-100 active:${color} uppercase font-sketch font-bold shadow-lg hover:shadow-xl`}
-          >
-            {children}
-          </button>
-        </Link>
+    externalUrl ? (
+      <a target='_blank' href={url} rel='noopener noreferrer'>
+        <button
+          className={`${width} ${color} ${hoverColor} text-white font-medium py-4 px-6 ${rounded} tracking-wider hover:scale-105 duration-150 active:scale-100 active:${color} uppercase font-work font-bold shadow-lg hover:shadow-xl`}
+        >
+          {children}
+        </button>
+      </a>
+    ) : (
+      <Link href={url}>
+        <button
+          className={`${color} ${hoverColor} text-white font-medium py-4 px-6 ${rounded} tracking-wider hover:scale-105 duration-150 active:scale-100 active:${color} uppercase font-work font-bold shadow hover:shadow-md`}
+        >
+          {children}
+        </button>
+      </Link>
     )
   ) : (
     <button
-      className={`${width} ${color} ${hoverColor} text-white font-medium py-4 px-6 ${rounded} tracking-wider text-lg  duration-150 active:scale-100 active:${color} uppercase font-sketch font-bold shadow-md  ${disabled ? 'opacity-70 cursor-default' : 'hover:shadow-lg hover:scale-105 cursor-pointer'
-        }`}
+      className={`${width} ${color} ${hoverColor} text-white font-medium py-4 px-6 ${rounded} tracking-wider  duration-150 active:scale-100 active:${color} uppercase font-work font-bold shadow-md  ${
+        disabled ? 'opacity-70 cursor-default' : 'hover:shadow-lg hover:scale-105 cursor-pointer'
+      }`}
     >
       {children}
     </button>
