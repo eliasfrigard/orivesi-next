66cec88064a294990063d1a323551bd3f184d615
eliasfrigard <elias_frigard@hotmail.com>
2023-04-14 20:48:01 +0300

    less default padding

--- components/Layout.js
+++ components/Layout.js
@@ -5,7 +5,7 @@ export default function Layout({ children }) {
   return (
     <div className='bg-primary-500'>
       <Navbar />
-      <main className='pt-[135px] min-h-[calc(90vh)]'>{children}</main>
+      <main className='pt-[83px] min-h-[calc(90vh)]'>{children}</main>
       <Footer />
     </div>
   )
