9d579ee237b4f0932e4fca4e887590ceb6a62ab5
Elias Frigård <elias.frigard@tvtools.fi>
2023-04-15 08:51:06 +0300

    fix frontpage event centering

--- components/Modules/EventHighlights.js
+++ components/Modules/EventHighlights.js
@@ -6,18 +6,16 @@ export default function EventHighlights({ events }) {
   return (
     <div className='flex flex-col	items-center mt-16 md:mt-32 md:mb-16 text-grey-500'>
       <Title version='v1'>SEURAAVAT TAPAHTUMAT</Title>
-      <div className='max-w-full grid grid-flow-row xl:grid-cols-3 lg:grid-cols-2 w-full gap-10 items-center my-16 sm:mb-20 '>
+      <div className='max-w-full grid grid-flow-row xl:grid-cols-3 lg:grid-cols-2 w-full gap-10 items-center justify-center my-16 sm:mb-20 '>
         {events.map((event) => (
           <EventPreview
-            key={event.id}
             link={event.slug}
-            title={event.attributes.Title}
             date={event.attributes.Start}
+            title={event.attributes.Title}
             location={event.attributes.Location}
             city={event.attributes.City}
             country={event.attributes.Country}
-            startTime={event.attributes.Start}
-            // endTime={event.attributes.End}
+            key={event.attributes.id}
           ></EventPreview>
         ))}
         {events.length <= 0 ? (
