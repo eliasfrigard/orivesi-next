23b934fdcd9dbd1867e0e636cb98ff4895c4cd6c
Elias Frigård <elias.frigard@tvtools.fi>
2023-05-18 00:32:05 +0300

    less DRY tiltedimages

--- components/Modules/TiltedImages.js
+++ components/Modules/TiltedImages.js
@@ -1,35 +1,23 @@
 import Image from 'next/image'
 import AnimateIn from '../AnimateIn'
 
-export default function ShortPresentation() {
+export default function TiltedImages() {
+  const renderTiltedImage = (src, delay, tilt) => (
+    <AnimateIn delay={delay} opacityDuration={1000}>
+      <div className={`relative aspect-[9/10] w-44 flex-none overflow-hidden rounded-xl sm:w-72 sm:rounded-2xl ${tilt === 'left' ? 'rotate-2' : '-rotate-2'} shadow-md`}>
+        <Image alt='top-slice' src={src} layout='fill' objectFit='cover' />
+      </div>
+    </AnimateIn>
+  )
+
   return (
     <div className='hidden lg:block mb-32'>
       <div className='-my-4 flex justify-center gap-5 overflow-hidden py-4 sm:gap-8'>
-        <AnimateIn opacityDuration={1000}>
-          <div className='relative aspect-[9/10] w-44 flex-none overflow-hidden rounded-xl sm:w-72 sm:rounded-2xl rotate-2 shadow-md'>
-            <Image alt='top-slice' src='/tilted-images/2.webp' layout='fill' objectFit='cover' />
-          </div>
-        </AnimateIn>
-        <AnimateIn delay={200} opacityDuration={1000}>
-          <div className='relative aspect-[9/10] w-44 flex-none overflow-hidden rounded-xl sm:w-72 sm:rounded-2xl -rotate-2 shadow-md'>
-            <Image alt='top-slice' src='/tilted-images/1.webp' layout='fill' objectFit='cover' />
-          </div>
-        </AnimateIn>
-        <AnimateIn delay={400} opacityDuration={1000}>
-          <div className='relative aspect-[9/10] w-44 flex-none overflow-hidden rounded-xl sm:w-72 sm:rounded-2xl rotate-2 shadow-md'>
-            <Image alt='top-slice' src='/tilted-images/4.webp' layout='fill' objectFit='cover' />
-          </div>
-        </AnimateIn>
-        <AnimateIn delay={600} opacityDuration={1000}>
-          <div className='relative aspect-[9/10] w-44 flex-none overflow-hidden rounded-xl sm:w-72 sm:rounded-2xl rotate-2 shadow-md'>
-            <Image alt='top-slice' src='/tilted-images/3.webp' layout='fill' objectFit='cover' />
-          </div>
-        </AnimateIn>
-        <AnimateIn delay={800} opacityDuration={1000}>
-          <div className='relative aspect-[9/10] w-44 flex-none overflow-hidden rounded-xl sm:w-72 sm:rounded-2xl -rotate-2 shadow-md'>
-            <Image alt='top-slice' src='/tilted-images/5.webp' layout='fill' objectFit='cover' />
-          </div>
-        </AnimateIn>
+        {renderTiltedImage('/tilted-images/2.webp', 0, 'left')}
+        {renderTiltedImage('/tilted-images/1.webp', 200, 'right')}
+        {renderTiltedImage('/tilted-images/4.webp', 400, 'left')}
+        {renderTiltedImage('/tilted-images/3.webp', 600, 'right')}
+        {renderTiltedImage('/tilted-images/5.webp', 800, 'left')}
       </div>
     </div>
   )
